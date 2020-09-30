e4b3a6088004f7959f5a2407a8652ae04089fe9f
Daniel.Huang <Daniel.Huang@infortrend.com>
2020-09-30 11:24:15 +0800

    clear-all

--- build/deploy-dev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Making js ugly."
-for f in js/*.js ; do npm run uglifyjs -- --compress --mangle -o "$f" -- "$f" ; done
--- node/generate-quick-reference.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-const utB = require("./util-book-reference");
-
-const my_path = path.join("../data/generated", "bookref-quick.json");
-fs.writeFileSync(my_path, JSON.stringify(utB.UtilBookReference.getIndex({name: "快速參照", id: "bookref-quick", tag: "quickref"})).replace(/\s*\u2014\s*?/g, "\\u2014"), "utf8");
-console.log("Updated Quick references.");
